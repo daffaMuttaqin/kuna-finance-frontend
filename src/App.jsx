8edccf0d2537491ff07c60d6675fd34f9d3462b0
Daffa Muttaqin <nine@upi.edu>
2025-07-22 15:46:48 +0700

    Fungsi Login done

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/dashboard";
 import ProtectedRoutes from "./components/ProtectedRoute";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Home />} />
         <Route
           path="/dashboard"
           element={
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,22 @@
+import { useNavigate } from "react-router-dom";
+
 const Dashboard = () => {
-  return <h2 style={{ textAlign: "center" }}>Selamat Datang di Dashboard</h2>;
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    navigate("/login");
+  };
+  return (
+    <div>
+      <h2>Dashboard</h2>
+      <button
+        onClick={handleLogout}
+        className="px-4 py-2 bg-blue-500 rounded-lg"
+      >
+        Logout
+      </button>
+    </div>
+  );
 };
 
 export default Dashboard;
