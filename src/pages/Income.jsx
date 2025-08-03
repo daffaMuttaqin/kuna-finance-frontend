3bc400f4720c62e4ca304ae7c049edadd7aeac7e
daffaMuttaqin <daffamuttaqinwork@gmail.com>
2025-08-03 18:23:59 +0700

    Page Income done

--- src/pages/Income.jsx
+++ src/pages/Income.jsx
@@ -10,6 +10,11 @@ function Income() {
   const [purchaseDate, setPurchaseDate] = useState("");
   const [totalPrice, setTotalPrice] = useState("");
   const [notes, setNotes] = useState("");
+  const [isEditing, setIsEditing] = useState(false);
+  const [editingId, setEditingId] = useState(null);
+  const [toastMessage, setToastMessage] = useState("");
+  const [deleteId, setDeleteId] = useState(null);
+  const [searchQuery, setSearchQuery] = useState("");
 
   // List manual barang
   const items = [
@@ -64,23 +69,45 @@ function Income() {
     };
 
     try {
-      const res = await API.post("/incomes", payload);
+      if (isEditing && editingId) {
+        // EDIT DATA
+        const res = await API.put(`/incomes/${editingId}`, payload);
+        const now = new Date().toISOString();
+        const updatedIncome = {
+          ...res.data,
+          item_name: selectedItem.value,
+          customer_name: selectedCustomer.label,
+          notes: notes,
+          total_price: parseInt(totalPrice),
+          purchase_date: res.data.purchase_date || purchaseDate || now,
+          created_at: res.data.created_at || now,
+        };
 
-      // Normalisasi data untuk menghindari error NaN dan Invalid Date
-      const now = new Date().toISOString();
-      // Tambah data baru ke dalam state `data` (tanpa fetch ulang)
-      const newIncome = {
-        ...res.data,
-        customer_name: selectedCustomer.label,
-        item_name: selectedItem.value,
-        notes: notes,
-        total_price: Number(res.data.total_price) || parseInt(totalPrice),
-        purchase_date: res.data.purchase_date || purchaseDate || now,
-        created_at: res.data.created_at || now,
-      };
+        setData((prev) =>
+          prev.map((item) => (item.id === editingId ? updatedIncome : item))
+        );
 
-      // Tambahkan data baru ke state
-      setData((prev) => [...prev, newIncome]);
+        setToastMessage("Data telah berhasil diedit");
+      } else {
+        // Tambah Data
+        const res = await API.post("/incomes", payload);
+        // Normalisasi data untuk menghindari error NaN dan Invalid Date
+        const now = new Date().toISOString();
+        // Tambah data baru ke dalam state `data` (tanpa fetch ulang)
+        const newIncome = {
+          ...res.data,
+          customer_name: selectedCustomer.label,
+          item_name: selectedItem.value,
+          notes: notes,
+          total_price: Number(res.data.total_price) || parseInt(totalPrice),
+          purchase_date: res.data.purchase_date || purchaseDate || now,
+          created_at: res.data.created_at || now,
+        };
+
+        // Tambahkan data baru ke state
+        setData((prev) => [...prev, newIncome]);
+        setToastMessage("Data telah berhasil ditambah");
+      }
 
       // Reset form
       setSelectedItem(null);
@@ -104,6 +131,59 @@ function Income() {
     }
   };
 
+  // Reset Form
+  const resetForm = () => {
+    setSelectedItem(null);
+    setSelectedCustomer(null);
+    setPurchaseDate("");
+    setTotalPrice("");
+    setNotes("");
+    setIsEditing(false);
+    setEditingId(null);
+  };
+
+  const parseDateToLocalInput = (dateString) => {
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = `${date.getMonth() + 1}`.padStart(2, "0");
+    const day = `${date.getDate()}`.padStart(2, "0");
+    return `${year}-${month}-${day}`;
+  };
+
+  // EDIT DATA
+  const handleEdit = (item) => {
+    setIsEditing(true);
+    setEditingId(item.id);
+
+    setSelectedItem({ label: item.item_name, value: item.item_name }); // karena manual
+    setSelectedCustomer({ label: item.customer_name, value: item.customer_id });
+    setPurchaseDate(parseDateToLocalInput(item.purchase_date));
+    setTotalPrice(item.total_price);
+    setNotes(item.notes || "");
+
+    document.getElementById("modal_tambah_barang").showModal();
+  };
+
+  // DELETE DATA
+  const handleDelete = async () => {
+    try {
+      await API.delete(`/incomes/${deleteId}`);
+      setData((prev) => prev.filter((item) => item.id !== deleteId));
+      document.getElementById("modal_delete").close();
+
+      // Tampilkan toast
+      setToastMessage("Data berhasil dihapus");
+      const toast = document.getElementById("toast-success");
+      toast.classList.remove("hidden");
+      setTimeout(() => {
+        toast.classList.add("hidden");
+      }, 3000);
+    } catch (err) {
+      console.error(err);
+      alert("Gagal menghapus data");
+    }
+  };
+
   return (
     <div className="w-full h-full">
       <div className="p-0 lg:p-6">
@@ -111,7 +191,7 @@ function Income() {
           <h2 className="text-2xl font-bold mb-4">Pemasukan</h2>
           <div className="flex">
             {/* Input Search */}
-            <div className="lg:block hidden">
+            <div className="lg:block ">
               <label className="input">
                 <svg
                   className="h-[1em] opacity-50"
@@ -129,15 +209,24 @@ function Income() {
                     <path d="m21 21-4.3-4.3"></path>
                   </g>
                 </svg>
-                <input type="search" required placeholder="Cari nama" />
+                <input
+                  type="search"
+                  value={searchQuery}
+                  onChange={(e) => setSearchQuery(e.target.value)}
+                  required
+                  placeholder="Cari nama"
+                />
               </label>
             </div>
+            {/* End Search */}
+
             {/* Tambah income */}
             <button
               className="btn btn-info ml-2"
-              onClick={() =>
-                document.getElementById("modal_tambah_barang").showModal()
-              }
+              onClick={() => {
+                resetForm();
+                document.getElementById("modal_tambah_barang").showModal();
+              }}
             >
               Tambah
             </button>
@@ -160,82 +249,101 @@ function Income() {
               </tr>
             </thead>
             <tbody>
-              {data.map((item, key) => (
-                <tr key={item.id || `temp-${key}`}>
-                  <th> {key + 1} </th>
-                  <td> {item.item_name} </td>
-                  <td> {item.customer_name} </td>
-                  <td>
-                    Rp.{" "}
-                    {item.total_price &&
-                      Number(item.total_price).toLocaleString("id-ID", {
-                        minimumFractionDigits: 0,
-                        maximumFractionDigits: 0,
-                      })}
-                  </td>
-                  <td className="text-center">
-                    {" "}
-                    {new Date(item.purchase_date).toLocaleString("id-ID", {
-                      day: "numeric",
-                      month: "long",
-                      year: "numeric",
-                    })}{" "}
-                  </td>
-                  <td className="text-center">
-                    {" "}
-                    {new Date(item.created_at).toLocaleString("id-ID", {
-                      day: "numeric",
-                      month: "long",
-                      year: "numeric",
-                    })}{" "}
-                  </td>
-                  <td> {item.notes} </td>
-                  <td className="flex justify-center">
-                    <button className="btn btn-square btn-success">
-                      <svg
-                        className="size-[22px]"
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        fill="none"
-                        viewBox="0 0 24 24"
+              {data
+                .filter((item) =>
+                  item.customer_name
+                    ?.toLowerCase()
+                    .includes(searchQuery.toLowerCase())
+                )
+                .map((item, key) => (
+                  <tr key={item.id || `temp-${key}`}>
+                    <th> {key + 1} </th>
+                    <td> {item.item_name} </td>
+                    <td> {item.customer_name} </td>
+                    <td>
+                      Rp.{" "}
+                      {item.total_price &&
+                        Number(item.total_price).toLocaleString("id-ID", {
+                          minimumFractionDigits: 0,
+                          maximumFractionDigits: 0,
+                        })}
+                    </td>
+                    <td className="text-center">
+                      {" "}
+                      {new Date(item.purchase_date).toLocaleString("id-ID", {
+                        day: "numeric",
+                        month: "long",
+                        year: "numeric",
+                      })}{" "}
+                    </td>
+                    <td className="text-center">
+                      {" "}
+                      {new Date(item.created_at).toLocaleString("id-ID", {
+                        day: "numeric",
+                        month: "long",
+                        year: "numeric",
+                      })}{" "}
+                    </td>
+                    <td> {item.notes} </td>
+                    <td className="flex justify-center">
+                      <button
+                        onClick={() => handleEdit(item)}
+                        className="btn btn-square btn-success"
                       >
-                        <path
-                          stroke="currentColor"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M10.779 17.779 4.36 19.918 6.5 13.5m4.279 4.279 8.364-8.643a3.027 3.027 0 0 0-2.14-5.165 3.03 3.03 0 0 0-2.14.886L6.5 13.5m4.279 4.279L6.499 13.5m2.14 2.14 6.213-6.504M12.75 7.04 17 11.28"
-                        />
-                      </svg>
-                    </button>
-                    <button className="btn btn-square btn-error lg:ml-2 ml-1">
-                      <svg
-                        className="size-[20px]"
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        fill="none"
-                        viewBox="0 0 24 24"
+                        <svg
+                          className="size-[22px]"
+                          aria-hidden="true"
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="24"
+                          height="24"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                        >
+                          <path
+                            stroke="currentColor"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M10.779 17.779 4.36 19.918 6.5 13.5m4.279 4.279 8.364-8.643a3.027 3.027 0 0 0-2.14-5.165 3.03 3.03 0 0 0-2.14.886L6.5 13.5m4.279 4.279L6.499 13.5m2.14 2.14 6.213-6.504M12.75 7.04 17 11.28"
+                          />
+                        </svg>
+                      </button>
+                      <button
+                        onClick={() => {
+                          setDeleteId(item.id); // simpan id yang akan dihapus
+                          document.getElementById("modal_delete").showModal();
+                        }}
+                        className="btn btn-square btn-error lg:ml-2 ml-1"
                       >
-                        <path
-                          stroke="currentColor"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z"
-                        />
-                      </svg>
-                    </button>
-                  </td>
-                </tr>
-              ))}
+                        <svg
+                          className="size-[20px]"
+                          aria-hidden="true"
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="24"
+                          height="24"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                        >
+                          <path
+                            stroke="currentColor"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z"
+                          />
+                        </svg>
+                      </button>
+                    </td>
+                  </tr>
+                ))}
             </tbody>
           </table>
         </div>
+        {/* END TABEL */}
       </div>
+
+      {/* MODAL */}
+
       {/* MODAL TAMBAH BARANG */}
       <dialog id="modal_tambah_barang" className="modal">
         <div className="modal-box">
@@ -302,13 +410,56 @@ function Income() {
 
             {/* Tombol Submit */}
             <button onClick={handleSubmit} className="btn btn-info mt-4">
-              Tambah
+              Submit
             </button>
           </fieldset>
         </div>
-        <form method="dialog" className="modal-backdrop">
+        <form method="dialog" className="modal-backdrop" onClick={resetForm}>
           <button>close</button>
         </form>
+      </dialog>
+
+      {/* MODAL DELETE ITEM */}
+      <dialog id="modal_delete" className="modal">
+        <div className="modal-box">
+          <div className="p-4 md:p-5 text-center">
+            <svg
+              className="mx-auto mb-4 text-gray-400 w-12 h-12 "
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 20 20"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+              />
+            </svg>
+            <h3 className="mb-5 text-lg font-normal ">
+              Apakah kamu yakin ingin menghapus data ini?
+            </h3>
+            <button
+              onClick={handleDelete}
+              type="button"
+              className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center"
+            >
+              Ya, Saya yakin
+            </button>
+            <button
+              onClick={() => {
+                setDeleteId(null);
+                document.getElementById("modal_delete").close();
+              }}
+              type="button"
+              className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100"
+            >
+              Tidak
+            </button>
+          </div>
+        </div>
       </dialog>
 
       {/* TOAST SUCCESS */}
@@ -329,9 +480,7 @@ function Income() {
           </svg>
           <span className="sr-only">Check icon</span>
         </div>
-        <div className="ms-3 text-sm font-normal">
-          Data berhasil ditambahkan.
-        </div>
+        <div className="ms-3 text-sm font-normal">{toastMessage}</div>
         <button
           type="button"
           className="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 "
