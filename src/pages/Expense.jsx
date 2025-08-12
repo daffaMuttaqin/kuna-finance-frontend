736c2dcf6160d2e1bb90df925b41be3257b6e115
daffaMuttaqin <daffamuttaqinwork@gmail.com>
2025-08-12 18:14:40 +0700

    Tambah fungsi get data expenses

--- src/pages/Expense.jsx
+++ src/pages/Expense.jsx
@@ -8,6 +8,8 @@ function Expense() {
     Operasional: 0,
     Marketing: 0,
   });
+  const [data, setData] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
 
   // Get data category perbulan
   useEffect(() => {
@@ -36,6 +38,19 @@ function Expense() {
         setCategoryData(updatedData);
       } catch (error) {
         console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  // Ambil Data Seluruh Expenses
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await API.get("/expenses");
+        setData(res.data);
+      } catch (error) {
+        console.error("Gagal mengambil data", error);
       }
     };
     fetchData();
@@ -92,6 +107,117 @@ function Expense() {
             </div>
           </div>
         </div>
+
+        {/* TABEL */}
+        <div className="h-[723px] overflow-x-auto">
+          <table className="table table-sm table-pin-rows">
+            <thead>
+              <tr>
+                <th></th>
+                <th>Nama Barang</th>
+                <th>Kategori</th>
+                <th className="text-center">Tanggal Pengeluaran</th>
+                <th>Total Harga</th>
+                <th>Keterangan</th>
+                <th className="text-center">Tanggal Ditambahkan</th>
+                <th className="text-center">Ditambahkan Oleh</th>
+                <th className="text-center">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data
+                .filter((item) =>
+                  item.item_name
+                    ?.toLowerCase()
+                    .includes(searchQuery.toLowerCase())
+                )
+                .map((item, key) => (
+                  <tr key={item.id || `temp-${key}`}>
+                    <th> {key + 1} </th>
+                    <td> {item.item_name} </td>
+                    <td> {item.category} </td>
+                    <td className="text-center">
+                      {" "}
+                      {new Date(item.expense_date).toLocaleString("id-ID", {
+                        day: "numeric",
+                        month: "long",
+                        year: "numeric",
+                      })}{" "}
+                    </td>
+                    <td>
+                      Rp.{" "}
+                      {item.total_price &&
+                        Number(item.total_price).toLocaleString("id-ID", {
+                          minimumFractionDigits: 0,
+                          maximumFractionDigits: 0,
+                        })}
+                    </td>
+                    <td> {item.notes} </td>
+                    <td className="text-center">
+                      {" "}
+                      {new Date(item.created_at).toLocaleString("id-ID", {
+                        day: "numeric",
+                        month: "long",
+                        year: "numeric",
+                      })}{" "}
+                    </td>
+                    <td> {item.notes} </td>
+
+                    <td className="flex justify-center">
+                      <button
+                        // onClick={() => handleEdit(item)}
+                        className="btn btn-square btn-success"
+                      >
+                        <svg
+                          className="size-[22px]"
+                          aria-hidden="true"
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="24"
+                          height="24"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                        >
+                          <path
+                            stroke="currentColor"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M10.779 17.779 4.36 19.918 6.5 13.5m4.279 4.279 8.364-8.643a3.027 3.027 0 0 0-2.14-5.165 3.03 3.03 0 0 0-2.14.886L6.5 13.5m4.279 4.279L6.499 13.5m2.14 2.14 6.213-6.504M12.75 7.04 17 11.28"
+                          />
+                        </svg>
+                      </button>
+                      <button
+                        // onClick={() => {
+                        //   setDeleteId(item.id); // simpan id yang akan dihapus
+                        //   document.getElementById("modal_delete").showModal();
+                        // }}
+                        className="btn btn-square btn-error lg:ml-2 ml-1"
+                      >
+                        <svg
+                          className="size-[20px]"
+                          aria-hidden="true"
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="24"
+                          height="24"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                        >
+                          <path
+                            stroke="currentColor"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z"
+                          />
+                        </svg>
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </div>
+        {/* END TABEL */}
       </div>
     </div>
   );
